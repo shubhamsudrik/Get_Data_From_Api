6b9b9623fa4bb7e04c331e8753698f6f941f690b
SHUBHAM SUDRIK <113041732+shub-2151@users.noreply.github.com>
2023-07-20 16:24:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { CoreBase, IMIRequest, IMIResponse, IUserContext, MIRecord } from '@info
 import { MIService, UserService } from '@infor-up/m3-odin-angular';
 
 
-/* 
+/*
 Take input from user for customer order number (ORNO)
 Use the ORNO and CONO as input for OIS100MI LstLine transaction
 Input CONO and ORNO
@@ -21,7 +21,8 @@ export class AppComponent extends CoreBase implements OnInit {
       userContext = {} as IUserContext;
       isBusy = false;
       company: string;
-   
+
+
       supplierNo :string;
       puNo:string;
       errormsg :string;
@@ -51,7 +52,7 @@ export class AppComponent extends CoreBase implements OnInit {
    }
 
    ngOnInit() {
-      
+
    }
    onClickLoad(): void {
      // console.log(this.puNo);
@@ -61,7 +62,7 @@ export class AppComponent extends CoreBase implements OnInit {
 
       if(this.company!==null){}
       this.logInfo('onClickLoad');
-      
+
       this.setBusy(true);
       this.userService.getUserContext().subscribe((userContext: IUserContext) => {
          this.setBusy(false);
@@ -77,34 +78,34 @@ export class AppComponent extends CoreBase implements OnInit {
 
    updateUserValues(userContext: IUserContext) {
       this.company = userContext.company;
-     
+
    }
 
    private setBusy(isBusy: boolean) {
       this.isBusy = isBusy;
    }
-   
+
    async callAllFunction() {
       this.OIS100MI_LstLine(this.company, this.orNo);
       this.MMS200MI_GetItmBasic(this.company, this.itNo);
     }
-    
+
     MMS200MI_GetItmBasic(company: string, itemnumber: string) {
       const inputRecord = new MIRecord();
       inputRecord.setNumber("CONO", company);
       inputRecord.setNumber("ITNO", itemnumber);
-    
+
       const request: IMIRequest = {
         program: "MMS200MI",
         transaction: "GetItmBasic",
         record: inputRecord,
         outputFields: ["ITDS"]
       };
-    
+
       this.setBusy(true);
       this.miService.execute(request).subscribe((response: IMIResponse) => {
         this.setBusy(false);
-    
+
         if (!response.hasError()) {
           const records: MIRecord[] = response.items as MIRecord[];
           records.forEach((record: MIRecord) => {
@@ -119,29 +120,29 @@ export class AppComponent extends CoreBase implements OnInit {
         console.log('Invalid Input')
       });
     }
-    
-    
-    
+
+
+
     OIS100MI_LstLine(company: string, CustomerOrderNumber: string) {
       const inputRecord = new MIRecord();
-    
+
       const companyName = company;
       inputRecord.setNumber("CONO", companyName);
-    
+
       const cOrderNumber = CustomerOrderNumber;
       inputRecord.setNumber("ORNO", cOrderNumber);
-    
+
       const request: IMIRequest = {
         program: "OIS100MI",
         transaction: "LstLine",
         record: inputRecord,
         outputFields: ["PONR", "POSX", "ITNO", "ORQT", "NLAM", "ITDS"] // outputFields
       };
-    
+
       this.setBusy(true);
       this.miService.execute(request).subscribe((response: IMIResponse) => {
         this.setBusy(false);
-    
+
         if (!response.hasError()) {
           this.lineitems = response.items as MIRecord[];
         }
@@ -149,5 +150,5 @@ export class AppComponent extends CoreBase implements OnInit {
            console.log('Invalid Input')
       });
     }
-    
-   }    
+
+   }
